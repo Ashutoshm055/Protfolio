f2833226e45c09e401a16fd27d818ef4ae8d1534
Ashutosh Mohapatra <ashutoshmohapatra@Ashutoshs-MacBook-Air.local>
2026-01-02 23:35:49 +0530

    clickable element with scroll debug

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const github = document.querySelector("#github");
 const ab = document.querySelector("#AB");
 const mainWrapper = document.querySelector("#main-wrapper");
 const menu = document.querySelector(".menu-icon");
+const clickable = document.querySelectorAll(".click");
 
 insta.addEventListener("click", () => {
     window.location.href = "https://www.instagram.com/theashutosh_mohapatra/";
@@ -39,5 +40,13 @@ menu.addEventListener("click", function(){
     document.body.classList.toggle('no-scroll');
 });
 
-
+// close the menu icon when a option is clicked
+clickable.forEach(item => {
+    item.addEventListener("click", function(){
+        this.classList.toggle('active');
+        menu.classList.toggle('active');
+        mainWrapper.classList.toggle('active');
+        document.body.classList.toggle('no-scroll');
+    });
+});
   
